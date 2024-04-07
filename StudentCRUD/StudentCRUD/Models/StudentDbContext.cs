81e4c55a2cd0b830b1689109c84a207f1d8da97d
Suvechha Roy <suvechharoy06@gmail.com>
2024-04-07 18:39:33 +0530

    Add project files.

---
+++ StudentCRUD/StudentCRUD/Models/StudentDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentCRUD.Models
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Student { get;set; }
+    }
+}
